eae2c59230e2e119598d90cff3184c6fba20e9eb
bb <whynot.byoul@gmail.com>
2023-06-19 07:28:14 +0900

    layout id animate

--- src/App.tsx
+++ src/App.tsx
@@ -6,77 +6,40 @@ const Wrapper = styled(motion.div)`
   width: 100vw;
   height: 100vh;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  flex-direction: column;
+
   /* background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238)); */
 `;
 
 const Box = styled(motion.div)`
-  position: absolute;
-
-  top: 100px;
   width: 400px;
-  height: 200px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
 `;
 
-const box = {
-  entry: (back: boolean) => ({
-    x: back ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 1,
-    },
-  },
-  exit: (back: boolean) => ({
-    x: back ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: {
-      duration: 1,
-    },
-  }),
-};
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState(false);
-  const nextPlease = () => {
-    setBack(false);
-    setVisible((prev) => (prev === 10 ? 1 : prev + 1));
-  };
-  const prevPlease = () => {
-    setBack(true);
-    setVisible((prev) => (prev === 1 ? 10 : prev - 1));
-  };
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
   return (
-    <Wrapper>
-      <AnimatePresence custom={back}>
-        <Box
-          custom={back}
-          variants={box}
-          initial="entry"
-          animate="center"
-          exit="exit"
-          key={visible}
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <button onClick={prevPlease}>prev</button>
-      <button onClick={nextPlease}>next</button>
+    <Wrapper onClick={toggleClicked}>
+      <Box>{!clicked ? <Circle layoutId="circle" /> : null}</Box>
+      <Box>
+        {clicked ? <Circle layoutId="circle" style={{ scale: 2 }} /> : null}
+      </Box>
     </Wrapper>
   );
 }
